470cedd46d4662450c236704477491382c3ad16d
Amal Roy <amalroy1232@gmail.com>
2021-04-03 22:01:32 +0530

    Tasks component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: TasksComponent},
+{path: 'tasks', component: TasksComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
